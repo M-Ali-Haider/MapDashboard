f5f70d7ac8e56aa47f2cb3d72184b21c00bbe745
M-Ali-Haider <mah01134212100@gmail.com>
2024-07-22 15:12:52 +0500

    fixing progress bar

--- src/components/Dashboard/Job-Seeker/progressBar.jsx
+++ src/components/Dashboard/Job-Seeker/progressBar.jsx
@@ -1,13 +1,12 @@
 const ProfileProgressBar = () => {
-  const percentageFromResponse = 70;
-  const style = percentageFromResponse + "%";
+  const percentage = 70;
   return (
     <>
       <div className="flex items-center gap-2">
         <div
-          className={`relative w-full h-4 bg-[#DFDFDF] overflow-hidden rounded-full before:w-[${style}] before:rounded-full before:absolute before:bottom-0 before:right-0 before:left-0 before:top-0 before:bg-[#89A0FF]`}
+          className={`relative w-full h-4 bg-[#DFDFDF] overflow-hidden rounded-full before:w-[${percentage}*1%] before:rounded-full before:absolute before:bottom-0 before:right-0 before:left-0 before:top-0 before:bg-[#89A0FF]`}
         ></div>
-        <div>{percentageFromResponse}%</div>
+        <div>{percentage}%</div>
       </div>
     </>
   );

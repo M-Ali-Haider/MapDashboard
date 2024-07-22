f4b9a0baeb1a754102db40b5836d18257e478042
M-Ali-Haider <mah01134212100@gmail.com>
2024-07-22 14:59:18 +0500

    fixed profileChecks components

--- src/components/Dashboard/Job-Seeker/profileChecks.jsx
+++ src/components/Dashboard/Job-Seeker/profileChecks.jsx
@@ -1,14 +1,13 @@
 import ProfileCheckSVG from "@/assets/profileCheck";
 
 const ProfileChecks = ({ item }) => {
+  const color = item.checkStatus ? "#53BD00" : "#757575";
   return (
     <>
       <div className="flex items-center gap-2">
         <div className="text-[#757575]">{item.checkTitle}</div>
         <div>
-          <ProfileCheckSVG
-            className={`fill-[${item.checkStatus ? "#757575" : "#53BD00"}]`}
-          />
+          <ProfileCheckSVG fill={color} />
         </div>
       </div>
     </>

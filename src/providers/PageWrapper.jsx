eba22daef56b2c14b84b87554aa24416eb25f2e6
M-Ali-Haider <mah01134212100@gmail.com>
2024-07-23 13:44:20 +0500

    readjusting dashboard page

--- src/providers/PageWrapper.jsx
+++ src/providers/PageWrapper.jsx
@@ -7,10 +7,10 @@ const PageWrapper = ({ children }) => {
   return (
     <div
       className={`${
-        isSidebarOpen ? "px-9" : "md:px-2 px-5"
-      } pt-7 max-h-[calc(100vh-84px)] max-w-[1440px] overflow-y-scroll w-full  no-scrollbar transition-all duration-200 ease-custom-ease`}
+        isSidebarOpen ? "md:px-9" : "md:px-2"
+      } px-5 pt-7 max-h-[calc(100vh-84px)] max-w-[1440px] overflow-y-scroll w-full  no-scrollbar transition-all duration-200 ease-custom-ease`}
     >
-      <div className={`min-h-screen pb-8 px-10 md:px-0`}>{children}</div>
+      <div className={`min-h-screen pb-8 px-4 md:px-0`}>{children}</div>
     </div>
   );
 };
